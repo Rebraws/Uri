1bd48f57e2eb7a10d122b0286c68066f2425835e
Rebraws <nahuel.braian@gmail.com>
2022-05-20 23:21:33 -0300

    Format: Formatting testing source file

--- test/TestHelpFunctions.cpp
+++ test/TestHelpFunctions.cpp
@@ -5,11 +5,12 @@
 #include "UriTest.h"
 #include <AfterUri/parseFunctions.h>
 
-TEST(HelpFunc, extract_hostname) {
+TEST(HelpFunc, extract_hostname)
+{
   using namespace std::string_view_literals;
-  
+
   std::string authority = "www.example.com";
-  
+
   auto hostname = internalFunc::extract_hostname(authority);
   EXPECT_EQ(hostname, "www.example.com"sv);
 
@@ -20,15 +21,15 @@ TEST(HelpFunc, extract_hostname) {
   authority = "www.example.com:80";
   hostname = internalFunc::extract_hostname(authority);
   EXPECT_EQ(hostname, "www.example.com"sv);
-  
+
   authority = "rebraws@www.example.com:80";
   hostname = internalFunc::extract_hostname(authority);
   EXPECT_EQ(hostname, "www.example.com"sv);
-  
+
   authority = "username:password@subdomain.example.com:8000";
   hostname = internalFunc::extract_hostname(authority);
   EXPECT_EQ(hostname, "subdomain.example.com"sv);
-  
+
   authority = "[2001:0db8:85a3:0000:0000:8a2e:0370:7334]";
   hostname = internalFunc::extract_hostname(authority);
   EXPECT_EQ(hostname, "[2001:0db8:85a3:0000:0000:8a2e:0370:7334]"sv);
@@ -37,4 +38,3 @@ TEST(HelpFunc, extract_hostname) {
   hostname = internalFunc::extract_hostname(authority);
   EXPECT_EQ(hostname, "[2001:0db8:85a3:0000:0000:8a2e:0370:7334]"sv);
 }
-
--- test/UriTest.h
+++ test/UriTest.h
@@ -5,11 +5,13 @@
 #include <AfterUri/AfterUri.h>
 #include <vector>
 #include <string>
-class UriTest : public ::testing::Test {
+class UriTest : public ::testing::Test
+{
 public:
-  auto SetUp() -> void override;    
+  auto SetUp() -> void override;
 
-  struct TestCase {
+  struct TestCase
+  {
     std::string uri;
     std::string authority;
     std::string scheme;
@@ -19,12 +21,9 @@ public:
     bool has_port;
     std::uint16_t port;
     AfterUri::Error error;
-
   };
-  
+
   std::vector<TestCase> mTestCases;
-
-
 };
 
 
